15bfa973644aefc4d83e20df6567dd6a8a0f542f
Rob <77rgh77@gmail.com>
2025-12-05 21:03:54 -0500

    init iOS app in step 1

---
+++ step1/iOS/harmony-step1/harmony-step1/harmony_step1App.swift
@@ -0,0 +1,17 @@
+//
+//  harmony_step1App.swift
+//  harmony-step1
+//
+//  Created by Rob Hughes on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct harmony_step1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
